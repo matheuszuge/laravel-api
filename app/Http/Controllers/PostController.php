666e6cb7897b69498dbcf10bbe7548404557de87
matheuszuge <matheusrzuge@gmail.com>
2025-05-06 21:53:53 -0300

    Refactor: Update PostController and Post model, remove unused controllers and migrations
    - Removed unused controllers: AdminController, ApiController, HomeController, LoginController, UserController.
    - Added a backup file: PostController.backup.php.
    - Updated PostController and Post model with necessary changes.
    - Modified the posts table migration.
    - Removed the settings table migration.
    - Updated routes/web.php to reflect changes.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\models\Post;
 use Illuminate\Http\Request;
-use App\Models\Post;
 
 class PostController extends Controller
 {
-
-    public function create(Request $request)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
+        $posts = Post::all();
+        return $posts;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
         $new_post = [
             'title' => 'meu primeiro post',
             'content' => 'conteudo basico',
@@ -19,6 +31,52 @@ class PostController extends Controller
 
         $post = new Post($new_post);
         $post->save();
-        dd($post);
+
+        return 'Post criado com sucesso';
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = new Post();
+        $post = $post->find($id);
+        return $post;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $posts = Post::find($id)->update([
+            'author' => 'Matheus',
+            'title' => 'alterado',
+        ]);
+
+        return $posts;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id) {}
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::find($id)->delete();
+        return $post;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,11 @@
 <?php
 
-use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 
-//api routes
-Route::get('/', [ApiController::class, 'index']);
-
 Route::get('/posts/create', [PostController::class, 'create']);
-
-Route::get('/posts/get', [PostController::class, 'getPosts']);
-
-
-
-
-//home routes
-Route::get('home', [ApiController::class, 'index']);
-
-//admin routes
-Route::get('admin', [ApiController::class, 'index']);
-
-//users routes
-Route::get('users', [ApiController::class, 'index']);
+Route::get('/posts/read', [PostController::class, 'read']);
+Route::get('/posts/All', [PostController::class, 'all']);
+Route::get('/posts/update', [PostController::class, 'update']);
+Route::get('/posts/delete', [PostController::class, 'delete']);
